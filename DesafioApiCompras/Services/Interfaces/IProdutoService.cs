82620d51ee93f7070852b8e8e88144bd6e5ff862
Virna Oliveira <94839578+virnasoliveira@users.noreply.github.com>
2022-05-20 09:41:16 -0300

    Criação da controler e services.

---
+++ DesafioApiCompras/Services/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using DesafioApiCompras.Model;
+using System.Collections.Generic;
+
+namespace DesafioApiCompras.Services.Interfaces
+{
+    public interface IProdutoService
+    {
+        Produto Create(Produto produto);
+        Produto FindById(int id);
+        List<Produto> FindAll();
+        void Delete(int id);
+    }
+}
---
+++ DesafioApiCompras/Services/ProdutoService.cs
@@ -0,0 +1,59 @@
+﻿using DesafioApiCompras.Data;
+using DesafioApiCompras.Model;
+using DesafioApiCompras.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesafioApiCompras.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private ProdutoContexto _context;
+        public ProdutoService(ProdutoContexto context)
+        {
+            _context = context;
+        }
+
+        public List<Produto> FindAll()
+        {
+            return _context.Produtos.ToList();
+        }
+
+        public Produto FindById(int id)
+        {
+            return _context.Produtos.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Produto Create(Produto produto)
+        {
+            try
+            {
+                _context.Add(produto);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return produto;
+        }
+
+        public void Delete(int id)
+        {
+           var result = _context.Produtos.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Produtos.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
