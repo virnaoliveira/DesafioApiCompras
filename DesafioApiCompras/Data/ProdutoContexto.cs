5cc2c795c6a97deecd86e8dfe3fac44630b3790b
Virna Oliveira <94839578+virnasoliveira@users.noreply.github.com>
2022-05-20 08:52:48 -0300

    Integração com o banco de dados.

---
+++ DesafioApiCompras/Data/ProdutoContexto.cs
@@ -0,0 +1,22 @@
+﻿using DesafioApiCompras.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioApiCompras.Data
+{
+    public class ProdutoContexto : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cartao> Cartoes { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                ("Password=#jamsoftsistemas1310;Persist Security Info=True;User ID=sa;Initial Catalog=ApiCompras;Data Source=localhost\\sistemas");
+        }
+    }
+}
---
+++ DesafioApiCompras/Migrations/20220520114920_initial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesafioApiCompras.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cartoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    titular = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    numero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    data_expiracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    bandeira = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cvv = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cartoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    valor_unitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    qtde_estoque = table.Column<int>(type: "int", nullable: false),
+                    data_ultimavenda = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    valor_ultimavenda = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compras",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    produto_id = table.Column<int>(type: "int", nullable: false),
+                    qtde_comprada = table.Column<int>(type: "int", nullable: false),
+                    cartaoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compras", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Compras_Cartoes_cartaoId",
+                        column: x => x.cartaoId,
+                        principalTable: "Cartoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_cartaoId",
+                table: "Compras",
+                column: "cartaoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compras");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Cartoes");
+        }
+    }
+}
